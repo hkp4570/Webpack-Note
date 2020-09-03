8a5fd7d6f2b62756372ce6188cfabe1c470b32ec
hkp <sxhkp163@163.com>
2020-09-03 11:15:13 +0800

    loader使用

---
+++ webpack核心功能/loader/webpack.config.js
@@ -0,0 +1,36 @@
+//
+// module.exports = {
+//   mode: 'development',
+//   module: {
+//     rules: [
+//       {
+//         test:/index\.js$/,
+//         use:[
+//           {
+//             loader: './loaders/test-loader',
+//             options: {
+//               changeVar: '变量'
+//             }
+//           }
+//         ],
+//       },
+//     ]
+//   }
+// }
+
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /index\.js$/, //正则表达式，匹配模块的路径
+        use: ["./loaders/loader1", "./loaders/loader2"] //匹配到了之后，使用哪些加载器
+      }, //规则1
+      {
+        test: /\.js$/, //正则表达式，匹配模块的路径
+        use: ["./loaders/loader3", "./loaders/loader4"] //匹配到了之后，使用哪些加载器
+      } //规则2
+    ], //模块的匹配规则
+  }
+}
